fd478edd38f620e30542771dde61b7787d7e5f3b
jcruzz <jvcruz@ucdavis.edu>
2017-10-11 17:05:45 -0700

    finished linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (!list.head) { 
+      console.log(value);
+      list.head = new Node(value);
+      list.tail = list.head;
+    } else {
+      var node = new Node(value);
+      
+      list.tail.next = node;
+      
+      list.tail = node;
+    }
+
   };
 
   list.removeHead = function() {
+    var popped = list.head;
+    list.head = list.head.next;
+
+    return popped.value;
   };
 
   list.contains = function(target) {
+    for (var node = list.head; node != null; node = node.next) {
+      if (node.value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;

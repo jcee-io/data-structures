54f6fab2c5bddaca2ae05222f926c7046b8a04cd
jcruzz <jvcruz@ucdavis.edu>
2017-10-11 20:38:45 -0700

    trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,18 +3,43 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function(value) { //O(1)
+  var newTree = Tree(value);
+  this.children.push(newTree);
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target) { //O(n)
+  
+  var recurse = function(node) {
+  
+    if (node.value === target) {
+      return true;
+    }
+    
+    for (var i = 0; i < node.children.length; i++) {
+  
+      if (recurse(node.children[i])) {
+        return true;
+      }
+
+    }
+
+    
+  };
+  
+  
+  return recurse(this) ? true : false;
 };
+
+
 
 
 
